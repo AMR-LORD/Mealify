221b7b0fcf1582206429695c134657528d0ca529
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-04-09 14:22:23 +0200

    Add light and dark mode styles, theme toggle functionality, and update HTML structure

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const toggleButton = document.getElementById('toggleTheme');
+const rootElement = document.documentElement; // Cache the root element
+
+toggleButton.addEventListener('click', () => {
+  const currentTheme = rootElement.getAttribute('data-theme');
+  
+  if (currentTheme === 'dark') {
+    // إذا كان الوضع الحالي هو Dark Mode، نعود إلى Light Mode
+    rootElement.setAttribute('data-theme', 'light');
+  } else {
+    // إذا كان الوضع الحالي هو Light Mode، ننتقل إلى Dark Mode
+    rootElement.setAttribute('data-theme', 'dark');
+  }
+});
