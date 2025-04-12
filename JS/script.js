2ce8bda1a4700b94d19d1100e1ba5f7437fc86d4
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-04-12 22:19:17 +0200

    Implement dark mode styles and update theme toggle functionality

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,17 @@
-const toggleButton = document.getElementById('toggleTheme');
-const rootElement = document.documentElement; // Cache the root element
+// const toggleButton = document.getElementById('toggleTheme');
+// const rootElement = document.documentElement; // Cache the root element
 
-toggleButton.addEventListener('change', () => {
-  const currentTheme = rootElement.getAttribute('data-theme');
+// toggleButton.addEventListener('change', () => {
+//   const currentTheme = rootElement.getAttribute('data-theme');
   
-  if (currentTheme === 'dark') {
-    // إذا كان الوضع الحالي هو Dark Mode، نعود إلى Light Mode
-    rootElement.setAttribute('data-theme', 'light');
-  } else {
-    // إذا كان الوضع الحالي هو Light Mode، ننتقل إلى Dark Mode
-    rootElement.setAttribute('data-theme', 'dark');
-  }
-});
+//   if (currentTheme === 'dark') {
+//     // إذا كان الوضع الحالي هو Dark Mode، نعود إلى Light Mode
+//     rootElement.setAttribute('data-theme', 'light');
+//   } else {
+//     // إذا كان الوضع الحالي هو Light Mode، ننتقل إلى Dark Mode
+//     rootElement.setAttribute('data-theme', 'dark');
+//   }
+// });
 
 
 
